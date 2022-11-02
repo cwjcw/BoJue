af5595760f5a226477907d5cc5841646b57b9268
Jerry Cui <45133410+cwjcw@users.noreply.github.com>
2022-11-02 08:27:37 +0800

    Update and rename 打卡.py to 抓取企业微信打卡数据.py

--- 抓取企业微信打卡数据.py
+++ 抓取企业微信打卡数据.py
@@ -7,16 +7,14 @@ app = xw.App(visible=False,add_book=False)
 wb = app.books.open(r'C:\Users\Administrator\Desktop\出勤表.xlsx')
 sht = wb.sheets[0]
 
-ACCESS_TOKEN = 'bKvnpImGNH63TOugaaAft4u_iED4ckIXYM9ZBTI6frweALgnNMH8bFv_goOuZmM9PrpD9hIo6vRP-l2IQEz554i6M30tyf8h41R4BA' \
-               'zi5JG78_op6Vh6MBUQ4RygsvLDK_c7qPisCmk-8Qzjhkwu2pUEJ3iWgjpIU3aDmiFxrfZC4A70UCC0WcqgHzy4kZ60vAHT0Ja' \
-               'IJYpA37OF0LnbMQ'
+ACCESS_TOKEN = 'XXX'
 
 url = f"https://qyapi.weixin.qq.com/cgi-bin/checkin/getcheckindata?access_token={ACCESS_TOKEN}"
 
 id = ['U17330','U09928','U07338','U15303','U02835','U00364','U10670','U00011','U02073','U14228']
 
 headers = {
-   'Open-Authorization': '4VzqOE93a8ZeLvFcrATsRf2YuZThrVJempx1xTNY',
+   'Open-Authorization': 'XXXX',
    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36 QIHU 360SE',
    'Content-Type': 'application/json'
 }
